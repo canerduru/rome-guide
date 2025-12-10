2b5282413ef16bc2123d3940109d53939ebbfa68
owner <canerduru@ymail.com>
2025-12-11 00:42:55 +0300

    Configure base URL for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures assets are linked correctly on GitHub Pages
+  base: '/rome-guide/', // Ensures assets are linked correctly on GitHub Pages
 });
